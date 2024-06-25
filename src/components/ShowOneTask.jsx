30050347c1745d7287f56ceb7b5a32d88e9ee2aa
Carlos Miguel <85066001+ckzwebber@users.noreply.github.com>
2024-06-25 18:22:38 -0300

    Working

--- src/components/ShowOneTask.jsx
+++ src/components/ShowOneTask.jsx
@@ -6,18 +6,22 @@ import CheckboxOff from "./CheckboxOff";
 const ShowOneTask = ({ showTheTask, handleTaskClick, deleteTask }) => {
   return (
     <div className="tasks">
-      <div
-        className="show-one-task"
-        onClick={() => handleTaskClick(showTheTask.id)}
-      >
+      <div className="show-one-task">
         {showTheTask.title}
-        <button
-          className="delete-button"
-          onClick={() => deleteTask(showTheTask.id)}
-        >
-          x
-        </button>
-        {showTheTask.completed ? <Checkbox /> : <CheckboxOff />}
+        <div className="controls">
+          <button
+            className="delete-button"
+            onClick={() => deleteTask(showTheTask.id)}
+          >
+            X
+          </button>
+          <div
+            className="check"
+            onClick={() => handleTaskClick(showTheTask.id)}
+          >
+            {showTheTask.completed ? <Checkbox /> : <CheckboxOff />}
+          </div>
+        </div>
       </div>
     </div>
   );
